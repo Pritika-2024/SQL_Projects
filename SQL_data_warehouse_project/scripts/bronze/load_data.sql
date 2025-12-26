1c5e6d6a714ce95484d4c8c6a914b5c6da64361d
Pritika <mediboinapritika@gmail.com>
2025-12-26 17:59:01 +0530

    Create load_data.sql

---
+++ SQL_data_warehouse_project/scripts/bronze/load_data.sql
@@ -0,0 +1,59 @@
+/* loading data in csv file of local computer to empty tables*/
+
+TRUNCATE TABLE [bronze].[crm_cust_info]
+BULK INSERT [bronze].[crm_cust_info]
+FROM 'path\source_crm\cust_info.csv'
+WITH (
+    FIRSTROW =2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+
+TRUNCATE TABLE [bronze].[crm_prd_info]
+BULK INSERT [bronze].[crm_prd_info]
+FROM 'path\source_crm\prd_info.csv'
+WITH (
+    FIRSTROW =2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+
+TRUNCATE TABLE [bronze].[crm_sales_details]
+BULK INSERT [bronze].[crm_sales_details]
+FROM 'path\source_crm\sales_details.csv'
+WITH (
+    FIRSTROW =2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+
+TRUNCATE TABLE [bronze].[erp_px_cat_g1v2]
+BULK INSERT [bronze].[erp_px_cat_g1v2]
+FROM 'path\source_erp\PX_CAT_G1V2.csv'
+WITH (
+    FIRSTROW =2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+TRUNCATE TABLE [bronze].[erp_loc_a101]
+BULK INSERT [bronze].[erp_loc_a101]
+FROM 'path\source_erp\LOC_A101.csv'
+WITH (
+    FIRSTROW =2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+
+TRUNCATE TABLE [bronze].[erp_cust_az12]
+BULK INSERT [bronze].[erp_cust_az12]
+FROM 'path\source_erp\CUST_AZ12.csv'
+WITH (
+    FIRSTROW =2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
